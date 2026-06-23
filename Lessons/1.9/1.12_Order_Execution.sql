e5a5b1a0a1b794b113543d3add5dec30628d137f
Shalin Vaidya <shalinvaidya.17@gmail.com>
2026-06-23 04:26:39 +0200

    Add in remaining files

---
+++ Lessons/1.9/1.12_Order_Execution.sql
@@ -0,0 +1,49 @@
+/* 
+Find the top 10 companies for posting jobs
+They must have >3000 postings
+Limit this to only US jobs
+*/
+
+SELECT 
+    cd.name AS company_name,
+    COUNT (jpf.job_id) AS postings_count
+FROM 
+    job_postings_fact AS jpf 
+        LEFT JOIN company_dim AS cd 
+        ON jpf.company_id = cd.company_id
+WHERE 
+    jpf.job_country = 'United States'
+GROUP BY cd.name
+Having COUNT(jpf.job_id) > 3000
+ORDER BY postings_count  DESC;
+
+
+EXPLAIN
+SELECT 
+    cd.name AS company_name,
+    COUNT (jpf.job_id) AS postings_count
+FROM 
+    job_postings_fact AS jpf 
+        LEFT JOIN company_dim AS cd 
+        ON jpf.company_id = cd.company_id
+WHERE 
+    jpf.job_country = 'United States'
+GROUP BY cd.name
+Having COUNT(jpf.job_id) > 3000
+ORDER BY postings_count  DESC;
+
+
+EXPLAIN ANALYZE
+SELECT 
+    cd.name AS company_name,
+    COUNT (jpf.job_id) AS postings_count
+FROM 
+    job_postings_fact AS jpf 
+        LEFT JOIN company_dim AS cd 
+        ON jpf.company_id = cd.company_id
+WHERE 
+    jpf.job_country = 'United States'
+GROUP BY cd.name
+Having COUNT(jpf.job_id) > 3000
+ORDER BY postings_count  DESC;
+
